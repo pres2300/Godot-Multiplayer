5030ebd159bfe4280796d3b5124973841565d979
Jacob Preston <pres2300@gmail.com>
2025-11-01 22:10:11 -0700

    adding pressure plate with synchronization

---
+++ interactables/pressure_plate/pressure_plate.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var is_down: bool = false
+
+@onready var plate_up: Sprite2D = $PlateUp
+@onready var plate_down: Sprite2D = $PlateDown
+
+var bodies_on_plate: int = 0
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	if not multiplayer.is_server():
+		return
+
+	bodies_on_plate += 1
+	update_plate_state()
+
+func _on_area_2d_body_exited(_body: Node2D) -> void:
+	if not multiplayer.is_server():
+		return
+
+	bodies_on_plate -= 1
+	update_plate_state()
+
+func update_plate_state() -> void:
+	is_down = bodies_on_plate > 0
+	set_plate_properties()
+
+func set_plate_properties() -> void:
+	plate_down.visible = is_down
+	plate_up.visible = !is_down
+
+func _on_multiplayer_synchronizer_delta_synchronized() -> void:
+	set_plate_properties()
