4f9dc1fec66af67ba15acfd35bdbab37dcb08b4c
Jacob Preston <pres2300@gmail.com>
2025-11-24 21:10:15 -0800

    player can collect key; key follows player

--- interactables/chest/chest.gd
+++ interactables/chest/chest.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @export var is_locked: bool = true
+@export var key_scene: PackedScene
 
 @onready var chest_locked: Sprite2D = $ChestLocked
 @onready var chest_unlocked: Sprite2D = $ChestUnlocked
+@onready var key_spawn: Marker2D = $KeySpawnLocation
 
 func set_chest_properties():
 	chest_locked.visible = is_locked
@@ -15,6 +17,8 @@ func _on_interactable_interacted():
 
 	if is_locked:
 		is_locked = false
+		var key = key_scene.instantiate()
+		key_spawn.add_child(key)
 		set_chest_properties()
 
 func _on_multiplayer_synchronizer_delta_synchronized() -> void:
---
+++ interactables/key_door/key.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var follow_offset: Vector2 = Vector2(-50, -60)
+@export var lerp_speed: float = 5.0
+
+var following_body
+
+func _process(delta):
+	if multiplayer.is_server():
+		if following_body != null:
+			global_position = lerp(
+				following_body.global_position+follow_offset,
+				global_position,
+				pow(0.5, delta*lerp_speed)
+			)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	following_body = body
